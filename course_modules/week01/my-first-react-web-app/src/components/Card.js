eae517180f03949b8f496a1c3b353cb1ed62e4dd
icancss <iscnkr@gmail.com>
2022-02-17 20:44:20 +0100

    week 1 - my first react web app

---
+++ course_modules/week01/my-first-react-web-app/src/components/Card.js
@@ -0,0 +1,22 @@
+//Card function to create each of card design
+const Card = () => {
+  return (
+    <div className="card shadow-lg p-5 m-5 col-lg-3">
+      <svg class="bd-placeholder-img card-img-top" width="100%" height="180" 
+      xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Image cap" 
+      preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title>
+      <rect width="100%" height="100%" fill="#868e96"></rect>
+      <text x="33%" y="50%" fill="#dee2e6" dy=".3em">Image cap</text></svg>
+      <div className="card-body">
+        <h5 className="card-title">Card title</h5>
+        <p className="card-text">
+          Some quick example text to build on the card title and make up the
+          bulk of the card's content.
+        </p>
+        <button className="btn btn-primary">Go somewhere</button>
+      </div>
+    </div>
+  );
+};
+
+export default Card;
