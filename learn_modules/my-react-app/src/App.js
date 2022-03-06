ceea8ac3c3fe89b2778934720f2eb6e866f8f839
icancss <iscnkr@gmail.com>
2022-03-06 11:24:45 +0100

    some minor changes

---
+++ learn_modules/my-react-app/src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import ClickApp from "./components/ClickApp";
+
+function App() {
+  return (
+    <div className="App">
+      <ClickApp />
+    </div>
+  );
+}
+
+export default App;
